f6cda5335da25a4f2b920a75da0998dc7517fbf3
bukas <422867074@qq.com>
2018-11-02 11:26:57 +0800

    xiugai

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,6 +1,10 @@
 <template>
-    <div>
-        这是产品页面
+    <div class="warp">
+        <ul class="minnav">
+            <li>全部</li>
+            <li>借现金</li>
+            <li>信用卡</li>
+        </ul>
         
     </div>
 
@@ -15,5 +19,24 @@ export default {
 </script>
 
 <style lang="less">
+    *{
+        margin: 0;
+        padding: 0;
+    }
+    .warp{
+        width: 100%;
+        height: 100%;
+        .minnav{
+            display: flex;
+            width: 5.96rem;
+            height: 0.6667rem;
+            border-radius: 50%;
+            margin: 0 auto;
+            li{
+                flex: 1;
+                
+            }
+        }
 
+    }
 </style>
