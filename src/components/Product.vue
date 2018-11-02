7d698e45363532bc2aa65cb4e705c6b018383fe2
bukas <422867074@qq.com>
2018-11-02 11:49:03 +0800

    xiugai

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -5,6 +5,12 @@
             <li>借现金</li>
             <li>信用卡</li>
         </ul>
+        <ul class="subMeau">
+            <li>综合指数</li>
+            <li>成功率</li>
+            <li>放款速度</li>
+            <li>贷款利率</li>
+        </ul>
         
     </div>
 
@@ -14,6 +20,16 @@
 
 <script>
 export default {
+    data(){
+        return{
+            
+        }
+    },
+    methods:{
+        blue(){
+            
+        }
+    }
 
 }
 </script>
@@ -30,12 +46,19 @@ export default {
             display: flex;
             width: 5.96rem;
             height: 0.6667rem;
-            border-radius: 50%;
+            border-radius:0.6667rem;
             margin: 0 auto;
+            background-color: #ccc;
+            text-align: center;
+            line-height: 0.6667rem;
+            margin-bottom: 0.1333rem;
             li{
                 flex: 1;
-                
+
             }
+        }
+        .subMeau{
+            
         }
 
     }
