58330495499324aaf759d4c25a64836b389329f6
zhnagyx <18434901053@163.com>
2018-11-05 09:36:43 +0800

    提交

--- src/components/Mycenter.vue
+++ src/components/Mycenter.vue
@@ -1,12 +1,17 @@
 <template>
     <div>
         这是个人中心
+        
+        <Myfooter></Myfooter>
     </div>
 </template>
 
 <script>
+import Myfooter from '@/components/Myfooter.vue'
 export default {
-
+    components:{
+        Myfooter
+    }
 }
 </script>
 
--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
         这是产品页面
+        <Myfooter></Myfooter>
     </div>
 </template>
 
 <script>
+import Myfooter from '@/components/Myfooter.vue'
 export default {
-
+    components:{
+        Myfooter
+    }
 }
 </script>
 
