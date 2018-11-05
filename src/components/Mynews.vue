a866c44297a4241228aba8c99f31540888eea152
zhnagyx <18434901053@163.com>
2018-11-05 11:54:53 +0800

    提交

--- src/components/Mynews.vue
+++ src/components/Mynews.vue
@@ -1,6 +1,14 @@
 <template>
     <div>
-        消息页面
+        <header>
+            <span><</span>
+            <ul>
+                <li>进度查询</li>
+                <li>还款提醒</li>
+                <li>通知</li>
+            </ul>
+            <p></p>
+        </header>
     </div>
 </template>
 
@@ -9,7 +17,26 @@ export default {
 
 }
 </script>
-
-<style>
-
+<style lang="less" scoped>
+        header{
+            display: flex;
+            width:100%;
+            height:1.28rem /* 96/75 */ /* 9/75 */;
+            background: #1e2e45;
+            justify-content: space-around;
+            align-items: center;
+            color: #ffffff;
+            ul{
+                display: flex;
+                width: 6rem /* 450/75 */;
+                height: .666667rem /* 50/75 */;
+                background: #3e4d63;
+                border-radius: .133333rem /* 10/75 */;
+                li{
+                    width: .346667rem /* 26/75 */;
+                    height: 100%;
+                }
+            }
+        }
 </style>
+
